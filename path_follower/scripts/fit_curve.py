ee8569e7214bc8ed9920e70fac4c031f5af97cc8
KatieButler <katiethetraveller@gmail.com>
2017-03-09 16:53:23 -0500

    fit_curve!

---
+++ path_follower/scripts/fit_curve.py
@@ -0,0 +1,10 @@
+""" This is a script takes information from open_cv and does parameterizing 
+	math to follow the path """
+
+from cv_bridge import CvBridge
+import cv2
+import numpy as np
+from scipy.spatial.distance import cdist
+
+
+if __name__ == '__main__':
